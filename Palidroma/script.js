13c7f029bd566408161c5dd76e422892d5dcff99
vincaf <vincenzocafassog@gmail.com>
2022-06-22 17:01:20 +0200

    while loop to write the word backwards

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -3,9 +3,23 @@
 // Creare una funzione per capire se la parola inserita è palindroma.
 
 const userWord = prompt('inserisci una parola');
+console.log('Hai inserito la parola: ' + userWord);
 
 // Dichiarazione variabile singolo carattere della parola
 let char;
 
 // Dichiarazione variabile parola inversa
 let invertedWord = "";
+
+// Inizializzazione variabile index per ciclo while sulla parola inserita
+let i = userWord.length - 1;
+
+// Ciclo while per spezzare la parola e ricostruirla invertita
+while (i >= 0) {
+  char = userWord[i];
+  invertedWord += char;
+  console.log(char);
+  i--;
+}
+
+console.log(invertedWord);
