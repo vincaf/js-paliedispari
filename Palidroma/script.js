57bff508f56748e9afdd4d29af6503906e6af0f1
vincaf <vincenzocafassog@gmail.com>
2022-06-22 16:54:21 +0200

    variable declaration

---
+++ Palidroma/script.js
@@ -0,0 +1,11 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+const userWord = prompt('inserisci una parola');
+
+// Dichiarazione variabile singolo carattere della parola
+let char;
+
+// Dichiarazione variabile parola inversa
+let invertedWord = "";
